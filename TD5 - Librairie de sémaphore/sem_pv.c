7b20c16b23e1290acc8b7c85af7ba5c8e8cb3808
Batboooy <theo.bocquelet@yahoo.fr>
2016-05-11 18:19:58 +0200

    MAJ sem_pv

--- TD5 - Librairie de sémaphore/sem_pv.c
+++ TD5 - Librairie de sémaphore/sem_pv.c
@@ -1,7 +1,24 @@
 #include "semaph.h"
 
-int init_semaphore(){
+int  semid;
 
+int init_semaphore(){
+	int shmflag=IPC_CREAT|IPC_EXCL; 
+	int n_sem; // a initialiser
+	semid = semget(IPC_PRIVATE,n_sem,shmflag); // utiliser errno.h pour le cas ou le semaphore existe deja
+	if(semid==-1) {
+		perror("Creation echoue");
+		return -2;
+	}
 	
-	semget(IPC_PRIVATE,N_SEM,IPC_CREAT|IPC_EXCL);
+	return 0; // tout va bien, semaphore cree
+}
+
+int detruire_semaphore() {
+	semctl(semid,0,IPC_RMID);
+	return 0;
+}
+
+int main(){
+	return 0;
 }
