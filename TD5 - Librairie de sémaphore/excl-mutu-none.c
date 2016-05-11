41f66b43c82e5f8fd1710bfe13b97246f9c73dc6
Batboooy <theo.bocquelet@yahoo.fr>
2016-05-11 17:32:31 +0200

    Creation .c exercice 2

--- TD5 - Librairie de sémaphore/excl-mutu-none.c
+++ TD5 - Librairie de sémaphore/excl-mutu-none.c
@@ -13,28 +13,47 @@ int main(){
 	int *ptr;
 	ptr=shmat(shmid,0,shmflag); // attachement du segment
 
-	int e=0;
-	ptr[0]=e; // le segment se manipule sous forme de tableau 
+	int E=0;
+	ptr[0]=E; // le segment se manipule sous forme de tableau 
 
 	pid_t fils=fork();
 
 	int i,j;
+// variables du fils
+	int a;
+	int tps;
+// variables du pere
+	int A;
+	int TPS;
 
 	switch (fils){
 		case -1:
 			perror("Probleme fork");
 			break;	
 		case 0: // fils	
-			int a;
-			int tps;
 			for (i = 0; i < 100; i++)
 			{
 				a=ptr[0];
 				tps=rand()%(100-20)+20; // genere un temps d'attente entre 20 et 100
 				usleep(tps);
+				a++;
+				ptr[0]=a;
+				tps=rand()%(100-20)+20; 
+				usleep(tps);
 			}
 			break;
 		default: // pere
+			for (j = 0; j < 100; j++)
+			{
+				A=ptr[0];
+				TPS=rand()%(100-20)+20; 
+				usleep(TPS);
+				A++;
+				ptr[0]=A;
+				TPS=rand()%(100-20)+20; 
+				usleep(TPS);
+				printf("Iteration %d | E=%d\n",j,ptr[0]);
+			}
 	}
 
 	return 0;
