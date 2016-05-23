89316c1e7c5281166b501cadbf90b1b035df0021
Batboooy <theo.bocquelet@yahoo.fr>
2016-05-23 14:12:50 +0200

    MAJ exercice 3 - all gooooood
    tout fonctionne parfaitement cette fois !

--- TD5 - Librairie de sémaphore/prod-conso.c
+++ TD5 - Librairie de sémaphore/prod-conso.c
@@ -4,89 +4,66 @@
 #include "sem_pv.h"
 #define PROT 0666
 
-int nvide=5;  // nombre d'emplacements libres dans la memoire
-int nplein=0; // nombre de messages en attente 
+int nbVide=5;  // nombre d'emplacements libres dans la memoire
+int nbPlein=0; // nombre de messages en attente 
 
-int sem_places_libres=0;
-int sem_places_occupees=1;
+int sem_vide=0;
+int sem_plein=1;
 
 int main(){
 
 	int shmflag=PROT|IPC_CREAT; 
 	// shmget cree un segment de memoire partagee et retourne son id
 	int shmid;
-	shmid = shmget(IPC_PRIVATE,512,shmflag); // creation du segment memoire de taille 5 entiers
+	shmid = shmget(IPC_PRIVATE,5*sizeof(int),shmflag); // creation du segment memoire de taille 5 entiers
 	if(shmid == -1) perror("Creation echoue");
 
-	int *ptr;
-	ptr=shmat(shmid,0,shmflag); // attachement du segment
+	int *idx_w; // index d'ecriture
+	int *idx_r; // index de lecture
+	idx_w=shmat(shmid,0,shmflag);
+	idx_r=shmat(shmid,0,shmflag);
 
 	pid_t fils;
-	int i, j=0, k, l=0, v;
+	int i, j=0, k, l=0, var;
 
 	init_semaphore();
 	// initialisation
-	val_sem(sem_places_libres,nvide); // nvide
-	val_sem(sem_places_occupees,nplein); // nplein
+	val_sem(sem_vide,nbVide); 
+	val_sem(sem_plein,nbPlein); 
 
 	switch (fils=fork()){
 		case -1:
 			perror("Probleme fork");
 			break;	
 		case 0: // fils	- producteur
-			for (i = 1; i < 21; i++)
+			for (i = 1; i < 51; i++)
 			{
-				nvide -= 1;
-				// il n'y a pas de place disponible, on attend
-				if(nvide == -1) {
-					P(sem_places_libres);
-				}
-				// il y a de la place, on ecrit une lettre sur le buffer
-				else  {
-					ptr[j]=i;
-					printf("Ajoutee : %d\n", ptr[j]);
-					j++;
-					j = j%5;
-					nplein += 1;
-					val_sem(sem_places_occupees,nplein);
-				}
-				// si le consommateur est en attente, on le reveil
-				if(nplein == 0) {
-					V(sem_places_occupees);
-				}
-
+				// var = rand()%(50-1)+1;
+				P(sem_vide);
+				idx_w[j]=i;
+				printf("Ajoutee : %d\n", idx_w[j]);
+				j++;
+				j = j%5;
+				V(sem_plein);
 			}
-			shmdt(ptr);
+			shmdt(idx_w);
 			break;
 		default: // pere - consommateur
-			for (k = 1; k < 21; k++)
+			for (k = 1; k < 51; k++)
 			{
-				// il n'y a pas de message, on attend
-				nplein -= 1;
-				if(nplein == -1) {
-					P(sem_places_occupees);
-				}
-				// il y a un message en attente, on le recupere
-				else {
-					v=ptr[l];
-					printf("Consommee : %d\n", v);	
-					l++;
-					l = l%5;
-					nvide += 1;
-					val_sem(sem_places_libres,nvide);
-				}
-				// si le producteur est en attente, on le reveil
-				if(nvide == 0) {
-					V(sem_places_libres);
-				}
+				P(sem_plein);		
+				printf("Consommee : %d\n", idx_r[l]);	
+				l++;
+				l = l%5;
+				V(sem_vide);
 			}
 			wait(0); // on attend la fin du fils
-			shmdt(ptr); // detachement du segment memoire
+			shmdt(idx_r); // detachement du segment memoire
 			shmctl(shmid, IPC_RMID,0); // destruction du segment memoire
 			printf("Segment memoire supprime.\n");
 			detruire_semaphore();
+	
 	}
-
 	return 0;
 }
 
